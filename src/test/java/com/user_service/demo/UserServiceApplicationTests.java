85244ba7b7d082e9886a2650a41cc7ebc0b31d35
rsquare729 <rsquare729@gmail.com>
2026-05-13 22:57:49 +0530

    added jenkins pipeline

--- src/test/java/com/user_service/demo/UserServiceApplicationTests.java
+++ src/test/java/com/user_service/demo/UserServiceApplicationTests.java
@@ -3,7 +3,9 @@ package com.user_service.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.user_service.main.UserServiceApplication;
+
+@SpringBootTest(classes = UserServiceApplication.class)
 class UserServiceApplicationTests {
 
 	@Test
