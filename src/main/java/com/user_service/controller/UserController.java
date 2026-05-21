62c3a769ec5eb338d03007035aa6f32ef3bb0a22
rsquare729 <rsquare729@gmail.com>
2026-05-22 03:34:31 +0530

    jenking webhook

--- src/main/java/com/user_service/controller/UserController.java
+++ src/main/java/com/user_service/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.user_service.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.user_service.dto.UserRequestDto;
